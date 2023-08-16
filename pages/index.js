220240c596b5c998c18ef3cbafe27492c8ce8c63
jhonatang7 <jgpandia1@gmail.com>
2023-08-15 20:44:33 -0400

    add shadcn ui configuration

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <Button>Thornia</Button>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
