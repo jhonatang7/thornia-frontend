e08363c518ee1b6617fb11ed478f49891ea1a130
Sofia Toro <sofie.torch@outlook.com>
2023-09-03 18:07:04 -0400

    add toast when user registration fails

--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,8 @@
-import { ArrowLeft } from "lucide-react"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import * as zod from "zod"
+import { ArrowLeft, Loader2 } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+import { md5 } from "@/utils/md5";
 
 import {
   Form,
@@ -10,25 +11,83 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast"
+import { NextResponse } from "next/server";
+import { useState } from "react";
 
-const signUpFormSchema = zod.object({
-  name: zod.string({ required_error: "Introduce tu nombre" }).min(3, { message: "Debe tener al menos 3 caracteres" }).max(50, { message: "No debe pasar de 50 caracteres" }),
-  email: zod.string({ required_error: "Introduce tu correo" }).email({ message: "Ingresa un correo válido" }),
-  password: zod.string({ required_error: "Introduce una contraseña" }).regex(/^.*(?=.{6,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/, { message: "Debe tener al menos 6 caracteres, una letra, un dígito, un caracter especial" }),
-  confirmPassword: zod.string({ required_error: "Reescribe tu contraseña" })
-}).required().refine((data) => data.password === data.confirmPassword, {
-  message: "Las contraseñas no coinciden",
-  path: ["confirmPassword"],
-});
+const signUpFormSchema = zod
+  .object({
+    name: zod
+      .string({ required_error: "Introduce tu nombre" })
+      .min(3, { message: "Debe tener al menos 3 caracteres" })
+      .max(50, { message: "No debe pasar de 50 caracteres" }),
+    email: zod
+      .string({ required_error: "Introduce tu correo" })
+      .email({ message: "Ingresa un correo válido" }),
+    password: zod
+      .string({ required_error: "Introduce una contraseña" })
+      .regex(/^.*(?=.{6,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/, {
+        message:
+          "Debe tener al menos 6 caracteres, una letra, un dígito, un caracter especial",
+      }),
+    confirmPassword: zod.string({ required_error: "Reescribe tu contraseña" }),
+  })
+  .required()
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Las contraseñas no coinciden",
+    path: ["confirmPassword"],
+  });
 
 export default function SignUp() {
-  const signUpForm = useForm({ resolver: zodResolver(signUpFormSchema), mode: "onChange", })
+  const [requestStatus, setRequestStatus] = useState("initial");
+  const { toast } = useToast()
 
-  function onSubmit(values) {
-    console.log(values)
+  const signUpForm = useForm({
+    resolver: zodResolver(signUpFormSchema),
+    mode: "onChange",
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  async function onSubmit(signUpFormValues) {
+    setRequestStatus("inProgress");
+    signUpFormValues.password = md5(signUpFormValues.password);
+    delete signUpFormValues.confirmPassword;
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_HOST}/users/new`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(signUpFormValues),
+        }
+      );
+
+      if (response.ok) {
+        setRequestStatus("success");
+        NextResponse.redirect("/index");
+      } else throw new Error();
+    } catch (error) {
+      setRequestStatus("failure");
+      let { id } = toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request."
+      })
+
+      console.log(error)
+      console.log(id)
+    }
   }
 
   return (
@@ -39,10 +98,15 @@ export default function SignUp() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center mt-4 mb-1">
         Bienvenido a Thornia
       </h1>
-      <h3 className="mb-6 text-center text-2xl">Crea una cuenta para iniciar</h3>
+      <h3 className="mb-6 text-center text-2xl">
+        Crea una cuenta para iniciar
+      </h3>
 
       <Form {...signUpForm}>
-        <form onSubmit={signUpForm.handleSubmit(onSubmit)} className="space-y-3 container max-w-md">
+        <form
+          onSubmit={signUpForm.handleSubmit(onSubmit)}
+          className="space-y-3 container max-w-md"
+        >
           <FormField
             control={signUpForm.control}
             name="name"
@@ -63,7 +127,7 @@ export default function SignUp() {
               <FormItem>
                 <FormLabel>Correo electrónico</FormLabel>
                 <FormControl>
-                  <Input {...field} placeholder="correo@ejemplo.com"/>
+                  <Input {...field} placeholder="correo@ejemplo.com" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -76,7 +140,7 @@ export default function SignUp() {
               <FormItem>
                 <FormLabel>Contraseña</FormLabel>
                 <FormControl>
-                  <Input {...field} type="password" placeholder="*********"/>
+                  <Input {...field} type="password" placeholder="*********" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -89,16 +153,35 @@ export default function SignUp() {
               <FormItem>
                 <FormLabel>Confirma tu contraseña</FormLabel>
                 <FormControl>
-                  <Input {...field} type="password" placeholder="*********"/>
+                  <Input {...field} type="password" placeholder="*********" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit" className="w-full">Crear cuenta</Button>
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={requestStatus === "inProgress"}
+          >
+            {requestStatus === "inProgress" && (
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            )}
+
+            {requestStatus !== "inProgress" ? (
+              <span>Crear cuenta</span>
+            ) : (
+              <span>Espera un momento...</span>
+            )}
+          </Button>
         </form>
       </Form>
-      <p className="text-center mt-2">¿Ya tienes una cuenta? <Button variant="link" className="p-1 font-bold">Inicia sesión</Button> </p>
+      <p className="text-center mt-2">
+        ¿Ya tienes una cuenta?
+        <Button variant="link" className="p-1 font-bold">
+          Inicia sesión
+        </Button>
+      </p>
     </div>
-  )
+  );
 }
