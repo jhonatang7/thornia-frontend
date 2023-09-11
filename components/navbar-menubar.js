8516c3af1c5571d9c249261ac3cb5dddaa32e313
Sofia Toro <sofie.torch@outlook.com>
2023-09-10 23:27:22 -0400

    add account page interface

--- components/navbar-menubar.js
+++ components/navbar-menubar.js
@@ -17,7 +17,10 @@ export function NavbarMenubar() {
         <MenubarTrigger onClick={() => setCurrentPage("account")}>
           Cuenta
           <Avatar className="w-5 h-5 p-0 ml-2 my-0">
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarImage
+              src="https://en.gravatar.com/avatar/d694c1a2e792a196fa88c4d8878b1f5e?d=retro"
+              alt="@shadcn"
+            />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
         </MenubarTrigger>
--- pages/account.js
+++ pages/account.js
@@ -1,3 +1,80 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Pencil } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export function Account() {
-  return <p>Account works!</p>;
+  return (
+    <div className="flex flex-col justify-center items-center p-8 space-y-4">
+      <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+        Información de tu cuenta
+      </h2>
+      <div className="relative">
+        <Avatar className="w-48 h-48">
+          <AvatarImage
+            src="https://en.gravatar.com/avatar/d694c1a2e792a196fa88c4d8878b1f5e?d=retro"
+            alt="@shadcn"
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="absolute bottom-1 right-1 rounded-full"
+            >
+              <Pencil className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Foto de perfil</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Actualizar</DropdownMenuItem>
+            <DropdownMenuItem className="text-destructive focus:text-destructive focus:bg-destructive/10">
+              Quitar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
+      <div className="grid max-w-md w-full items-center gap-1.5">
+        <Label htmlFor="name">Nombre</Label>
+        <div className="flex items-center space-x-2">
+          <Input type="text" id="name" disabled={true} />
+          <Button type="submit" variant="ghost">
+            Editar
+          </Button>
+        </div>
+      </div>
+      <div className="grid max-w-md w-full items-center gap-1.5">
+        <Label htmlFor="email">Correo electrónico</Label>
+        <div className="flex items-center space-x-2">
+          <Input type="email" id="email" disabled={true} />
+          <Button type="submit" variant="ghost">
+            Editar
+          </Button>
+        </div>
+      </div>
+      <Button type="submit" variant="outline" className="max-w-md w-full">
+        Cambiar contraseña
+      </Button>
+      <Button
+        type="submit"
+        variant="destructiveOutline"
+        className="max-w-md w-full"
+      >
+        Cerrar sesión
+      </Button>
+    </div>
+  );
 }
