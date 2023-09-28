f7d6a29e18aa567653f26f3c972795d8d4efaac9
jhonatang7 <jgpandia1@gmail.com>
2023-09-28 00:57:43 -0400

    add a base component ProyectInitial and page newproject

---
+++ pages/newproject.js
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import { ProyectInitial } from "@/components/new-proyect/proyect-initial";
+
+export default function NewProject() {
+  return (
+    <main className="flex flex-col">
+      <div className="flex my-12">
+        <div className="basis-[10%]" />
+        <Button
+          variant="outline"
+          className="basis-[10%] flex-none"
+          onClick={() => router.back()}
+        >
+          <ArrowLeft className="mr-2 h-4 w-4" /> Volver
+        </Button>
+        <h1 className="basis-[70%] text-end scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Nuevo proyecto
+        </h1>
+        <div className="basis-[10%]" />
+      </div>
+      <div className="flex justify-center ">
+        <ProyectInitial />
+      </div>
+    </main>
+  );
+}
